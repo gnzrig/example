862d449d50fb1d5fc2a6a45858ae5a42871806ca
gnzrig <fleanflean123@gmail.com>
2023-01-12 15:02:02 +0800

    Projecting ehnii huwisagchdig uusgesen

---
+++ app.js
@@ -0,0 +1,21 @@
+// Тоглогчийн ээлжийг хадгалах хувьсагч, нэгдүгээр тоглогчийг 0, хоёрдугаар тоглогчийг 1 гэж тэмдэглэе.
+var activePlayer = 0;
+
+// Тоглогчийн цуглуулсан оноог хадгалах хувьсагч
+var scores = [0, 0];
+
+// Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+var roundScore = 0;
+
+// Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1 - 6 гэсэн утгаын энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
+var dice = Math.floor(Math.random() * 6) + 1;
+
+
+// Программ эхлэхэд бэлтгэе.
+document.getElementById("score-0").textContent = 0;
+document.getElementById("score-1").textContent = 0;
+
+document.getElementById("current-0").textContent = 0;
+document.getElementById("current-1").textContent = 0;
+
+document.querySelector(".dice").style.display = "none";
