7b055aa3c51f574465bbbd16e6bba4f7fd835f48
gnzrig <fleanflean123@gmail.com>
2023-01-12 16:08:42 +0800

    Winner tuluwtei bolgono

--- app.js
+++ app.js
@@ -35,16 +35,47 @@ document.querySelector(".btn-roll").addEventListener('click', function(){
         roundScore = roundScore + diceNumber;
         document.getElementById('current-' + activePlayer).textContent = roundScore;
     } else {
-        roundScore = 0;
-
-        document.getElementById('current-' + activePlayer).textContent = 0;
-
-        activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active');
-
-        diceDom.style.display = "none";
+        switchToNextPlayer();
     }
 
+})
+
+// HOLD эвент листенэр
+document.querySelector('.btn-hold').addEventListener("click" , function(){
+    scores[activePlayer] = scores[activePlayer] + roundScore;
+
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+
+    if(scores[activePlayer] >= 100){
+        document.getElementById("name-" + activePlayer).textContent = "Winner !!!"
+
+        document.querySelector('.player-'+activePlayer + '-panel').classList.add('winner');
+
+        document.querySelector('.player-'+activePlayer + '-panel').classList.remove('active');
+    } else {
+        switchToNextPlayer();
+    }
+
+    document.getElementById('score-' + activePlayer).textContent = scores[activePlayer];
+
+})
+
+
+function switchToNextPlayer(){
+    roundScore = 0;
+
+    document.getElementById('current-' + activePlayer).textContent = 0;
+
+    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active');
+
+    diceDom.style.display = "none";
+}
+
+// New-game
+
+document.querySelector('.btn-new').addEventListener('click', function(){
+    
 })
