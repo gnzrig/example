9b93de5ba69db707b606f28274e956e98f33a797
gnzrig <fleanflean123@gmail.com>
2023-01-12 15:15:28 +0800

    Shoog shiddeg bolgox

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ var scores = [0, 0];
 var roundScore = 0;
 
 // Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй, 1 - 6 гэсэн утгаын энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-var dice = Math.floor(Math.random() * 6) + 1;
-
 
 // Программ эхлэхэд бэлтгэе.
 document.getElementById("score-0").textContent = 0;
@@ -18,4 +16,14 @@ document.getElementById("score-1").textContent = 0;
 document.getElementById("current-0").textContent = 0;
 document.getElementById("current-1").textContent = 0;
 
-document.querySelector(".dice").style.display = "none";
+var diceDom = document.querySelector(".dice");
+
+diceDom.style.display = "none";
+
+document.querySelector(".btn-roll").addEventListener('click', function(){
+    var diceNumber = Math.floor(Math.random() * 6) + 1;
+
+    diceDom.style.display = "block";
+    diceDom.src = 'dice-' + diceNumber + '.png';
+
+})
